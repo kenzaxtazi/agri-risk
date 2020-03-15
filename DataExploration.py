8d7aa290519bd90c0130a8933ade546eb829c3ae
Kenza Tazi <kt2015@ic.ac.uk>
2020-03-15 16:03:45 +0000

    agroclimatic_indicators() returns histograms

--- DataExploration.py
+++ DataExploration.py
@@ -8,8 +8,6 @@ import xarray as xr
 import cartopy.feature as cf
 
 
-sns.set(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
-
 filepath = '/Users/kenzatazi/Downloads/head_of_soils_recommendations_MGM.csv'
 
 filepath1 = '/Users/kenzatazi/Downloads/FAOSTAT_data_3-11-2020.csv'
@@ -72,83 +70,68 @@ def agroclimatic_indicators(filepath):
     df_raw =  pd.read_csv(filepath)
     
     # Frost days (days)
-    FD = pd.DataFrame({'FD': (df_raw[['FD-01-05-2010', 'FD-02-05-2010', 'FD-03-05-2010', 'FD-04-05-2010',
-                                      'FD-05-05-2010', 'FD-06-05-2010', 'FD-07-05-2010', 'FD-08-05-2010', 
-                                      'FD-09-05-2010', 'FD-10-05-2010', 'FD-11-05-2010', 'FD-12-05-2010', 
-                                      'FD-01-15-2010', 'FD-02-15-2010', 'FD-03-15-2010', 'FD-04-15-2010',
-                                      'FD-05-15-2010', 'FD-06-15-2010', 'FD-07-15-2010', 'FD-08-15-2010', 
-                                      'FD-09-15-2010', 'FD-10-15-2010']].values).flatten()})
-    FD = FD.melt(value_vars='FD')
+    FD = (df_raw[['FD-01-05-2010', 'FD-02-05-2010', 'FD-03-05-2010', 'FD-04-05-2010',
+                  'FD-05-05-2010', 'FD-06-05-2010', 'FD-07-05-2010', 'FD-08-05-2010', 
+                  'FD-09-05-2010', 'FD-10-05-2010', 'FD-11-05-2010', 'FD-12-05-2010', 
+                  'FD-01-15-2010', 'FD-02-15-2010', 'FD-03-15-2010', 'FD-04-15-2010',
+                  'FD-05-15-2010', 'FD-06-15-2010', 'FD-07-15-2010', 'FD-08-15-2010', 
+                  'FD-09-15-2010', 'FD-10-15-2010']].values).flatten()
 
     # Biological Effective Degree Days (°C)
-    BEDD =  pd.DataFrame({'BEDD': (df_raw[['BEDD-01-05-2010', 'BEDD-02-05-2010', 'BEDD-03-05-2010',
-                                           'BEDD-04-05-2010', 'BEDD-05-05-2010', 'BEDD-06-05-2010',
-                                           'BEDD-07-05-2010', 'BEDD-08-05-2010', 'BEDD-09-05-2010',
-                                           'BEDD-10-05-2010', 'BEDD-11-05-2010', 'BEDD-12-05-2010',
-                                           'BEDD-01-15-2010', 'BEDD-02-15-2010', 'BEDD-03-15-2010',
-                                           'BEDD-04-15-2010', 'BEDD-05-15-2010', 'BEDD-06-15-2010',
-                                           'BEDD-07-15-2010', 'BEDD-08-15-2010', 'BEDD-09-15-2010',
-                                           'BEDD-10-15-2010', 'BEDD-11-15-2010', 'BEDD-12-15-2010', 
-                                           'BEDD-01-25-2010', 'BEDD-02-25-2010', 'BEDD-03-25-2010',
-                                           'BEDD-04-25-2010', 'BEDD-05-25-2010', 'BEDD-06-25-2010',
-                                           'BEDD-07-25-2010', 'BEDD-08-25-2010', 'BEDD-09-25-2010', 
-                                           'BEDD-10-25-2010', 'BEDD-11-25-2010', 'BEDD-12-25-2010']].values).flatten()})
-    BEDD = BEDD.melt(value_vars='BEDD')
-
-    # Warm Spell Duration Index (days)
-    WSDI = pd.DataFrame({'WSDI': (df_raw[['WSDI-Q1-2010', 'WSDI-Q2-2010',
-                         'WSDI-Q3-2010', 'WSDI-Q4-2010']].values).flatten()})
-    WSDI = WSDI.melt(value_vars='WSDI') 
-
-    # Cold Spell Duration Index (days)
-    CSDI = pd.DataFrame({'CSDI': (df_raw[['CSDI-Q1-2010', 'CSDI-Q2-2010', 
-                        'CSDI-Q3-2010', 'CSDI-Q4-2010']].values).flatten()})
-    CSDI = CSDI.melt(value_vars='CSDI')                    
+    BEDD =  (df_raw[['BEDD-01-05-2010', 'BEDD-02-05-2010', 'BEDD-03-05-2010',
+                     'BEDD-04-05-2010', 'BEDD-05-05-2010', 'BEDD-06-05-2010',
+                     'BEDD-07-05-2010', 'BEDD-08-05-2010', 'BEDD-09-05-2010',
+                     'BEDD-10-05-2010', 'BEDD-11-05-2010', 'BEDD-12-05-2010',
+                     'BEDD-01-15-2010', 'BEDD-02-15-2010', 'BEDD-03-15-2010',
+                     'BEDD-04-15-2010', 'BEDD-05-15-2010', 'BEDD-06-15-2010',
+                     'BEDD-07-15-2010', 'BEDD-08-15-2010', 'BEDD-09-15-2010',
+                     'BEDD-10-15-2010', 'BEDD-11-15-2010', 'BEDD-12-15-2010', 
+                     'BEDD-01-25-2010', 'BEDD-02-25-2010', 'BEDD-03-25-2010',
+                     'BEDD-04-25-2010', 'BEDD-05-25-2010', 'BEDD-06-25-2010',
+                     'BEDD-07-25-2010', 'BEDD-08-25-2010', 'BEDD-09-25-2010', 
+                     'BEDD-10-25-2010', 'BEDD-11-25-2010', 'BEDD-12-25-2010']].values).flatten()
+  
+    WSDI = (df_raw[['WSDI-Q1-2010', 'WSDI-Q2-2010', 'WSDI-Q3-2010', 'WSDI-Q4-2010']].values).flatten()
+    CSDI = (df_raw[['CSDI-Q1-2010', 'CSDI-Q2-2010', 'CSDI-Q3-2010', 'CSDI-Q4-2010']].values).flatten()                
    
-    # Warm and Wet days (days)
-    WW = pd.DataFrame({'WW':(df_raw[['WW-Q1-2010', 'WW-Q2-2010', 'WW-Q3-2010',
-                       'WW-Q4-2010']].values).flatten()})
-    WW = WW.melt(value_vars='WW')
+    WW = (df_raw[['WW-Q1-2010', 'WW-Q2-2010', 'WW-Q3-2010', 'WW-Q4-2010']].values).flatten()
 
-    # Cumulative Wet Days (days)
-    CWD = pd.DataFrame({'CWD': (df_raw[['CWD-Q1-2010', 'CWD-Q2-2010', 'CWD-Q3-2010', 
-                        'CWD-Q4-2010']].values).flatten()})
-    CWD = CWD.melt(value_vars='CWD')
 
-    # Cumulative Frost Days (days)
-    CFD = pd.DataFrame({'CFD': (df_raw[['CFD-Q1-2010', 'CFD-Q2-2010', 'CFD-Q3-2010',
-                        'CFD-Q4-2010']].values).flatten()})
-    CFD = CFD.melt(value_vars='CFD')
+    CWD = (df_raw[['CWD-Q1-2010', 'CWD-Q2-2010', 'CWD-Q3-2010', 'CWD-Q4-2010']].values).flatten()
+    CFD = (df_raw[['CFD-Q1-2010', 'CFD-Q2-2010', 'CFD-Q3-2010', 'CFD-Q4-2010']].values).flatten()
+    CDD = (df_raw[['CDD-Q1-2010', 'CDD-Q2-2010', 'CDD-Q3-2010', 'CDD-Q4-2010']].values).flatten()
 
-    # Cumulative Dry Days (days)
-    CDD = pd.DataFrame({'CDD': (df_raw[['CDD-Q1-2010', 'CDD-Q2-2010', 'CDD-Q3-2010',
-                        'CDD-Q4-2010']].values).flatten()})
-    CDD = CDD.melt(value_vars='CDD')
+    #  Plots
+    fig1, axs1 = plt.subplots(2, 1)
+    axs1[0].hist(FD, bins=10, density=True, label='Frost Days')
+    axs1[0].legend(facecolor='white')
+    axs1[1].hist(WW, bins=50, density=True, label= 'Warm and Wet Days')
+    axs1[1].legend(facecolor='white')
+    axs1[1].set_xlabel('Days')
 
-    df = FD.append([WW], ignore_index=True)
 
-    # FacetGrid object
-    pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
-    g = sns.FacetGrid(df, row="variable", hue="variable", aspect=15, height=.5, palette=pal)
+    fig2, axs2 = plt.subplots(3, 1)
+    axs2[0].hist(CDD, bins=92, density=True, label= 'Cumulative Dry Days')
+    axs2[0].legend(facecolor='white')
+    axs2[1].hist(CFD, bins=92, density=True, label='Cumulative Frost Days')
+    axs2[1].legend(facecolor='white')
+    axs2[2].hist(CWD, bins=92, density=True, label='Cumulative Wet Days')
+    axs2[2].legend(facecolor='white')
+    axs2[2].set_xlabel('Days')
 
-    # Densities 
-    g.map(sns.kdeplot, "value", clip_on=False, shade=True, alpha=1, lw=1.5, bw=.2)
-    # g.map(sns.kdeplot, "value", clip_on=False, color="w", lw=2, bw=.2)
-    g.map(plt.axhline, y=0, lw=2, clip_on=False)
+    fig3, axs3 = plt.subplots(1, 1)
+    axs3.hist(BEDD, bins=110, density=True, label='Biological Effective Degree Days')
+    axs3.legend(facecolor='white')
+    axs3.set_xlabel('°C')
 
-    # Define and use a simple function to label the plot in axes coordinates
-    def label(x, color, label):
-        ax = plt.gca()
-        ax.text(0, .2, label, fontweight="bold", color=color,
-                ha="left", va="center", transform=ax.transAxes)
+    fig4, axs4 = plt.subplots(2, 1)
+    axs4[0].hist(CSDI, bins=40, density=True, label='Cold Spell Duration Index')
+    axs4[0].legend(facecolor='white')
+    axs4[1].hist(WSDI, bins=44, density=True, label='Warm Spell Duration Index')
+    axs4[1].legend(facecolor='white')
+    axs4[1].set_xlabel('Days')
+    plt.show()
 
-    g.map(label, "value")
-
-    # Remove axes details that don't play well with overlap
-    g.set_titles("")
-    g.set(yticks=[])
-    g.despine(bottom=True, left=True)
- 
 
 def soil_types(filepath):
     df_raw =  pd.read_csv(filepath)
