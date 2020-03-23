b1dc2f90523b5a3a69ec90ac464213838b6dbd1a
Kenza Tazi <kt2015@ic.ac.uk>
2020-03-23 16:14:27 +0000

    histograms of area rather than grid point

--- DataExploration.py
+++ DataExploration.py
@@ -7,15 +7,15 @@ import cartopy.crs as ccrs
 import xarray as xr
 import cartopy.feature as cf
 
+filepath1 = '/Users/kenzatazi/Downloads/head_of_soils_recommendations_MGM-2.csv'
+filepath2 = '/Users/kenzatazi/Downloads/climate_monthly_seasonal_2005_2010_with_spatial_attributes_filtered.csv'
 
-filepath = '/Users/kenzatazi/Downloads/climate_monthly_seasonal_2005_2010_with_spatial_attributes_filtered.csv'
 
-
-def historical_yield(filepath, year):
+def historical_yield(filepath1, year):
     """ Returns the global map of maize yield """
     
     # create dataframe of relevant variables
-    df_raw =  pd.read_csv(filepath)
+    df_raw =  pd.read_csv(filepath1)
     df = df_raw[['x','y','maize_a_' + year, 'iso3']]
 
     # calculate mean for global, LIFDC and US
@@ -60,11 +60,13 @@ def historical_yield(filepath, year):
     plt.show()
 
 
-def agroclimatic_indicators(filepath):
+def agroclimatic_indicators(filepath1):
     """ returns histograms of the different indicators for 2010"""
     # Data (look at spread in different places over one year)
 
-    df_raw =  pd.read_csv(filepath)
+    df_raw =  pd.read_csv(filepath1)
+    area_weights(df_raw, 'y')
+    w = (df_raw['area weights'].values).flatten()
     
     # Frost days (days)
     FD = (df_raw[['FD-01-05-2010', 'FD-02-05-2010', 'FD-03-05-2010', 'FD-04-05-2010',
@@ -98,53 +100,60 @@ def agroclimatic_indicators(filepath):
     CFD = (df_raw[['CFD-Q1-2010', 'CFD-Q2-2010', 'CFD-Q3-2010', 'CFD-Q4-2010']].values).flatten()
     CDD = (df_raw[['CDD-Q1-2010', 'CDD-Q2-2010', 'CDD-Q3-2010', 'CDD-Q4-2010']].values).flatten()
 
+    w_FD = np.tile(w, 22)
+    w_quaterly = np.tile(w, 4)
+    w_10day = np.tile(w, 36)
+
     #  Plots
     fig1, axs1 = plt.subplots(2, 1)
-    axs1[0].hist(FD, bins=10, density=True, label='Frost Days')
+    axs1[0].hist(FD, weights=w_FD, bins=10, density=True, label='Frost Days')
     axs1[0].legend(facecolor='white')
-    axs1[1].hist(WW, bins=50, density=True, label='Warm and Wet Days')
+    axs1[1].hist(WW, weights=w_quaterly, bins=50, density=True, label='Warm and Wet Days')
     axs1[1].legend(facecolor='white')
     axs1[1].set_xlabel('Days')
 
+
     fig2, axs2 = plt.subplots(3, 1)
-    axs2[0].hist(CDD, bins=92, density=True, label='Maximum Consecutive Dry Days')
+    axs2[0].hist(CDD, weights=w_quaterly, bins=92, density=True, label='Maximum Consecutive Dry Days')
     axs2[0].legend(facecolor='white')
-    axs2[1].hist(CFD, bins=92, density=True, label='Maximum Consecutive Frost Days')
+    axs2[1].hist(CFD, weights=w_quaterly, bins=92, density=True, label='Maximum Consecutive Frost Days')
     axs2[1].legend(facecolor='white')
-    axs2[2].hist(CWD, bins=92, density=True, label='Maximum Consecutive Wet Days')
+    axs2[2].hist(CWD, weights=w_quaterly, bins=92, density=True, label='Maximum Consecutive Wet Days')
     axs2[2].legend(facecolor='white')
     axs2[2].set_xlabel('Days')
 
     fig3, axs3 = plt.subplots(1, 1)
-    axs3.hist(BEDD, bins=110, density=True, label='Biological Effective Degree Days')
+    axs3.hist(BEDD, weights=w_10day, bins=110, density=True, label='Biological Effective Degree Days')
     axs3.legend(facecolor='white')
     axs3.set_xlabel('°C')
 
     fig4, axs4 = plt.subplots(2, 1)
-    axs4[0].hist(CSDI, bins=40, density=True, label='Cold Spell Duration Index')
+    axs4[0].hist(CSDI, weights=w_quaterly, bins=40, density=True, label='Cold Spell Duration Index')
     axs4[0].legend(facecolor='white')
-    axs4[1].hist(WSDI, bins=44, density=True, label='Warm Spell Duration Index')
+    axs4[1].hist(WSDI, weights=w_quaterly, bins=44, density=True, label='Warm Spell Duration Index')
     axs4[1].legend(facecolor='white')
     axs4[1].set_xlabel('Days')
     plt.show()
 
 
-def soil_types(filepath):
-    df_raw =  pd.read_csv(filepath)
+def soil_types(filepath1):
+    
+    df_raw =  pd.read_csv(filepath1)
+    area_weights(df_raw, 'y')
+
     df = df_raw['soil_types']
     plt.figure()
-    plt.title('Soil quality')
     plt.ylabel('Count')
     plt.xlabel('Soil type')
-    df.hist(bins=107, align='mid', rwidth=0.75)
+    df.hist(weights= df_raw['area weights'], bins=107, align='mid', density=True, rwidth=0.75)
     plt.show()
 
 
-def irrigation(filepath):
+def irrigation(filepath2):
     """ Returns map of irrigated and waterfed maize """
     
     # create dataframe of relevant variables
-    df_raw =  pd.read_csv(filepath)
+    df_raw =  pd.read_csv(filepath2)
     df_values = df_raw[['lon','lat','irrigation']]
 
     df_pv = df_values.pivot(index='lat', columns='lon')
@@ -166,27 +175,30 @@ def irrigation(filepath):
     plt.show()
 
 
-def elevation_slope(filepath):
+def elevation_slope(filepath2):
     """ Returns histograms of elevation and slope """
-    df_raw =  pd.read_csv(filepath)
+    df_raw =  pd.read_csv(filepath2)
+    
+    area_weights(df_raw, 'lat')
+    w = (df_raw['area weights'].values).flatten()
 
     elevation = (df_raw['elevation'].values).flatten()
     slope = (df_raw['slope'].values).flatten()
 
     #  Plots
     fig, axs = plt.subplots(2, 1)
-    axs[0].hist(elevation, bins=50, density=True, label='Elevation')
+    axs[0].hist(elevation, weights=w, bins=50, density=True, label='Elevation')
     axs[0].legend(facecolor='white')
     axs[0].set_xlabel('m')
 
-    axs[1].hist(slope, bins=50, density=True, label= 'Slope')
+    axs[1].hist(slope, weights=w, bins=50, density=True, label= 'Slope')
     axs[1].legend(facecolor='white')
     axs[1].set_xlabel('deg')
 
     plt.show()
 
 
-def climate_zones(filepath):
+def climate_zones(filepath1):
 
     """ bar chart with climate zones of haversted areas """
 
@@ -196,13 +208,24 @@ def climate_zones(filepath):
              'Boreal\n(oceanic)', 'Boreal\n(sub-\ncontinental)',
              'Boreal \n(continental)', 'Arctic']
     
-    df_raw =  pd.read_csv(filepath)
-    df = df_raw['climate_zones']
-    df_clean = df[df >= 0]
+    df_raw =  pd.read_csv(filepath1)
+    df = df_raw[['climate_zones', 'y']]
+    df_clean = df[df['climate_zones'] >= 0]
+    area_weights(df_clean, 'y')
 
-    hist, bin_edges = np.histogram(df_clean)
+    hist, bin_edges = np.histogram(df_clean['climate_zones'], weights=df_clean['area weights'], density=True)
+    print(hist)
 
     plt.figure()    
     plt.bar(names, hist)
     plt.title('Climate zones')
     plt.show()
+
+
+def area_weights(df, lat_column):
+    """ returns area weights for a given latitude """
+
+    theta1 = (abs(df[lat_column]) - 0.041667) * np.pi /180.0
+    theta2 = (abs(df[lat_column]) + 0.041667) * np.pi /180.0
+
+    df['area weights'] = (abs(np.cos(theta1)) - abs(np.cos(theta2)))
